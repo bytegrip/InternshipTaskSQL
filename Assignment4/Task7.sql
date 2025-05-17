fb7aee9ff0ee99e0656e67578ed44244fa0419fa
lumijiez <59575049+lumijiez@users.noreply.github.com>
2025-05-17 18:04:44 +0300

    Task 7

--- Assignment4/Task7.sql
+++ Assignment4/Task7.sql
@@ -1,18 +1,12 @@
 USE AdventureWorks2022;
 
-SELECT 
-    pc.Name AS Category,
-    psc.Name AS Subcategory,
-    AVG(p.ListPrice) AS AvgListPrice
-FROM 
-    Production.Product AS p
-JOIN 
-    Production.ProductSubcategory AS psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
-JOIN 
-    Production.ProductCategory AS pc ON psc.ProductCategoryID = pc.ProductCategoryID
-GROUP BY 
-    pc.Name, psc.Name
-ORDER BY 
-    pc.Name, psc.Name;
+SELECT p.ProductSubcategoryID, p.Name, p.ListPrice
+FROM Production.Product p
+WHERE ListPrice = (
+    SELECT MAX(ListPrice)
+    FROM Production.Product
+    WHERE ProductSubcategoryID = p.ProductSubcategoryID
+)
+AND p.ProductSubcategoryID IS NOT NULL;
 
--- find the average list price for each product category and subcategory
+-- most expensive product in each subcategory
