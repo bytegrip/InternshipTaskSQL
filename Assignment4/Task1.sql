1588f25eef7adb96b26bb318dce0c56b4ed97e46
lumijiez <59575049+lumijiez@users.noreply.github.com>
2025-05-17 17:58:44 +0300

    Task 1

--- Assignment4/Task1.sql
+++ Assignment4/Task1.sql
@@ -1,15 +1,12 @@
 USE AdventureWorks2022;
 
-SELECT 
-	st.TerritoryID,
-	SUM(soh.TotalDue) AS Total
-FROM
-	Sales.SalesOrderHeader AS soh
-JOIN 
-	Sales.SalesTerritory AS st ON soh.TerritoryID = st.TerritoryID
-GROUP BY
-	st.TerritoryID
-ORDER BY 
-	Total DESC
+SELECT e.BusinessEntityID, p.FirstName, p.LastName
+FROM HumanResources.EmployeePayHistory eph
+JOIN HumanResources.Employee e ON eph.BusinessEntityID = e.BusinessEntityID
+JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID
+WHERE eph.Rate > (
+    SELECT AVG(Rate) 
+    FROM HumanResources.EmployeePayHistory
+);
 
--- total sales amount for each sales territory, sorted by the highest total
+-- employees who earn more than the average salary
