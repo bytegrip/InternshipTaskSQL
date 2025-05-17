5f95325dd3a11e38346cb0c93cfa7612851e1ef9
lumijiez <59575049+lumijiez@users.noreply.github.com>
2025-05-17 18:10:40 +0300

    Task 1

--- Assignment5/Task1.sql
+++ Assignment5/Task1.sql
@@ -1,15 +1,41 @@
 USE AdventureWorks2022;
 
-SELECT 
-	st.TerritoryID,
-	SUM(soh.TotalDue) AS Total
-FROM
-	Sales.SalesOrderHeader AS soh
-JOIN 
-	Sales.SalesTerritory AS st ON soh.TerritoryID = st.TerritoryID
-GROUP BY
-	st.TerritoryID
-ORDER BY 
-	Total DESC
+BEGIN TRANSACTION;
 
--- total sales amount for each sales territory, sorted by the highest total
+BEGIN TRY
+    DECLARE @NewBusinessEntityID INT;
+
+    INSERT INTO Person.BusinessEntity DEFAULT VALUES;
+    SET @NewBusinessEntityID = SCOPE_IDENTITY();
+
+    INSERT INTO Person.Person (
+        BusinessEntityID, PersonType, NameStyle, FirstName, LastName
+    )
+    VALUES (
+        @NewBusinessEntityID, 'EM', 0, 'John', 'Morrison'
+    );
+
+    INSERT INTO HumanResources.Employee (
+        BusinessEntityID, NationalIDNumber, LoginID, JobTitle,
+        BirthDate, MaritalStatus, Gender, HireDate
+    )
+    VALUES (
+        @NewBusinessEntityID, '999999999', 'adventure-works\jmorrison',
+        'Technician', '1990-01-01', 'S', 'M', GETDATE()
+    );
+
+    INSERT INTO HumanResources.EmployeeDepartmentHistory (
+        BusinessEntityID, DepartmentID, ShiftID, StartDate
+    )
+    VALUES (
+        @NewBusinessEntityID, 1, 1, GETDATE()
+    );
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+    THROW;
+END CATCH;
+
+-- add new employee
