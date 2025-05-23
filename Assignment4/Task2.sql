491665b3dbbc8cb2da296f713efa3f5ae392ec60
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:06:22 +0300

    Update Task2.sql

--- Assignment4/Task2.sql
+++ Assignment4/Task2.sql
@@ -1,16 +1,27 @@
 USE AdventureWorks2022;
 
-SELECT c.CustomerID, COUNT(soh.SalesOrderID) AS OrderCount
-FROM Sales.SalesOrderHeader soh
-JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
-GROUP BY c.CustomerID
-HAVING COUNT(soh.SalesOrderID) > (
-    SELECT AVG(OrderCount) 
-    FROM (
-        SELECT COUNT(*) AS OrderCount
-        FROM Sales.SalesOrderHeader
-        GROUP BY CustomerID
-    ) AS CustomerOrders
-);
+SELECT 
+    c.CustomerID, 
+    COUNT(soh.SalesOrderID) AS OrderCount
+FROM 
+    Sales.SalesOrderHeader soh
+JOIN 
+    Sales.Customer c 
+    ON soh.CustomerID = c.CustomerID
+GROUP BY 
+    c.CustomerID
+HAVING 
+    COUNT(soh.SalesOrderID) > (
+        SELECT 
+            AVG(OrderCount) 
+        FROM (
+            SELECT 
+                COUNT(*) AS OrderCount
+            FROM 
+                Sales.SalesOrderHeader
+            GROUP BY 
+                CustomerID
+        ) AS CustomerOrders
+    );
 
 -- customers who placed more orders than the average number of orders per customer
