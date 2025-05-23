dce52af816457f40045f9bb8a2995ff91d66828c
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:06:49 +0300

    Update Task3.sql

--- Assignment4/Task3.sql
+++ Assignment4/Task3.sql
@@ -1,13 +1,23 @@
 USE AdventureWorks2022;
 
-SELECT Name, ListPrice
-FROM Production.Product
+SELECT 
+    Name, 
+    ListPrice
+FROM 
+    Production.Product
 WHERE ListPrice > ALL (
-    SELECT ListPrice
-    FROM Production.Product p
-    JOIN Production.ProductSubcategory sc ON p.ProductSubcategoryID = sc.ProductSubcategoryID
-    JOIN Production.ProductCategory c ON sc.ProductCategoryID = c.ProductCategoryID
-    WHERE c.Name = 'Components'
+    SELECT 
+    ListPrice
+    FROM 
+    Production.Product p
+    JOIN 
+        Production.ProductSubcategory sc 
+        ON p.ProductSubcategoryID = sc.ProductSubcategoryID
+    JOIN 
+        Production.ProductCategory c 
+        ON sc.ProductCategoryID = c.ProductCategoryID
+    WHERE 
+        c.Name = 'Components'
 );
 
 -- products more expensive than all components
