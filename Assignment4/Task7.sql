4966facdada31f6dbd9a8a641f0ab43a3d34be2e
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:09:37 +0300

    Update Task7.sql

--- Assignment4/Task7.sql
+++ Assignment4/Task7.sql
@@ -1,12 +1,19 @@
 USE AdventureWorks2022;
 
-SELECT p.ProductSubcategoryID, p.Name, p.ListPrice
-FROM Production.Product p
+SELECT 
+    p.ProductSubcategoryID, 
+    p.Name, 
+    p.ListPrice
+FROM 
+    Production.Product p
 WHERE ListPrice = (
-    SELECT MAX(ListPrice)
-    FROM Production.Product
-    WHERE ProductSubcategoryID = p.ProductSubcategoryID
-)
-AND p.ProductSubcategoryID IS NOT NULL;
+    SELECT 
+        MAX(ListPrice)
+    FROM 
+        Production.Product
+    WHERE 
+        ProductSubcategoryID = p.ProductSubcategoryID
+    )
+    AND p.ProductSubcategoryID IS NOT NULL;
 
 -- most expensive product in each subcategory
