aedd13f4b1a1492e18c814e7d5085c9762f9764b
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:07:10 +0300

    Update Task4.sql

--- Assignment4/Task4.sql
+++ Assignment4/Task4.sql
@@ -1,10 +1,15 @@
 USE AdventureWorks2022;
 
-SELECT ProductID, Name
-FROM Production.Product
+SELECT 
+    ProductID, 
+    Name
+FROM 
+    Production.Product
 WHERE ProductID NOT IN (
-    SELECT DISTINCT ProductID
-    FROM Sales.SalesOrderDetail
+    SELECT DISTINCT 
+        ProductID
+    FROM 
+        Sales.SalesOrderDetail
 );
 
 -- products that were never sold
