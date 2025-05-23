4e14ee97473b4bdbd38df994e8c0df1e8d9dc955
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:05:30 +0300

    Update Task1.sql

--- Assignment4/Task1.sql
+++ Assignment4/Task1.sql
@@ -1,9 +1,17 @@
 USE AdventureWorks2022;
 
-SELECT e.BusinessEntityID, p.FirstName, p.LastName
-FROM HumanResources.EmployeePayHistory eph
-JOIN HumanResources.Employee e ON eph.BusinessEntityID = e.BusinessEntityID
-JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID
+SELECT 
+    e.BusinessEntityID, 
+    p.FirstName, 
+    p.LastName
+FROM 
+    HumanResources.EmployeePayHistory eph
+JOIN 
+    HumanResources.Employee e 
+    ON eph.BusinessEntityID = e.BusinessEntityID
+JOIN 
+    Person.Person p 
+    ON e.BusinessEntityID = p.BusinessEntityID
 WHERE eph.Rate > (
     SELECT AVG(Rate) 
     FROM HumanResources.EmployeePayHistory
