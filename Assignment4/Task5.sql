af9d66e64c7a01e10fff5fe2a20aa360f330c4bc
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:08:01 +0300

    Update Task5.sql

--- Assignment4/Task5.sql
+++ Assignment4/Task5.sql
@@ -1,16 +1,32 @@
 USE AdventureWorks2022;
 
-SELECT DISTINCT e.BusinessEntityID, p.FirstName, p.LastName
-FROM HumanResources.EmployeeDepartmentHistory edh
-JOIN HumanResources.Employee e ON edh.BusinessEntityID = e.BusinessEntityID
-JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID
+SELECT DISTINCT 
+    e.BusinessEntityID, 
+    p.FirstName, 
+    p.LastName
+FROM 
+    HumanResources.EmployeeDepartmentHistory edh
+JOIN 
+    HumanResources.Employee e 
+    ON edh.BusinessEntityID = e.BusinessEntityID
+JOIN 
+    Person.Person p 
+    ON e.BusinessEntityID = p.BusinessEntityID
 WHERE edh.DepartmentID = (
-    SELECT TOP 1 edh2.DepartmentID
-    FROM Person.Person p2
-    JOIN HumanResources.Employee e2 ON p2.BusinessEntityID = e2.BusinessEntityID
-    JOIN HumanResources.EmployeeDepartmentHistory edh2 ON e2.BusinessEntityID = edh2.BusinessEntityID
-    WHERE p2.FirstName = 'David' AND p2.LastName = 'Bradley'
-    ORDER BY edh2.StartDate DESC
+    SELECT TOP 1 
+        edh2.DepartmentID
+    FROM 
+        Person.Person p2
+    JOIN 
+        HumanResources.Employee e2 
+        ON p2.BusinessEntityID = e2.BusinessEntityID
+    JOIN 
+        HumanResources.EmployeeDepartmentHistory edh2 
+        ON e2.BusinessEntityID = edh2.BusinessEntityID
+    WHERE 
+        p2.FirstName = 'David' AND p2.LastName = 'Bradley'
+    ORDER BY 
+        edh2.StartDate DESC
 );
 
 -- employees in the same department as 'David Bradley'
