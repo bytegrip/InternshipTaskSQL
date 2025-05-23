c4718066840499994c7351cc821643213e4fa321
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:00:32 +0300

    Update Task3.sql

--- Assignment3/Task3.sql
+++ Assignment3/Task3.sql
@@ -1,14 +1,16 @@
 USE AdventureWorks2022;
 
 SELECT 
-	pp.ProductID,
-	pp.Name
-FROM
-	Production.Product AS pp
+    pp.ProductID,
+    pp.Name
+FROM 
+    Production.Product AS pp
 LEFT JOIN 
-	Sales.SalesOrderDetail AS ssod ON pp.ProductID = ssod.ProductID
-WHERE 
-	ssod.ProductID IS NULL
+    Sales.SalesOrderDetail AS ssod ON pp.ProductID = ssod.ProductID
+GROUP BY 
+    pp.ProductID, pp.Name
+HAVING 
+    COUNT(ssod.SalesOrderID) = 0;
 
 -- list all products that have never been sold
 
