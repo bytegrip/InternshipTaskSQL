a1e328299ff20f2877197dbfb3a37a5232ec3d4b
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:09:03 +0300

    Update Task6.sql

--- Assignment4/Task6.sql
+++ Assignment4/Task6.sql
@@ -1,12 +1,19 @@
 USE AdventureWorks2022;
 
-SELECT DISTINCT soh.CustomerID
-FROM Sales.SalesOrderHeader soh
-JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+SELECT DISTINCT 
+    soh.CustomerID
+FROM 
+    Sales.SalesOrderHeader soh
+JOIN 
+    Sales.SalesOrderDetail sod 
+    ON soh.SalesOrderID = sod.SalesOrderID
 WHERE sod.ProductID IN (
-    SELECT ProductID
-    FROM Production.Product
-    WHERE ListPrice > 1000
+    SELECT 
+        ProductID
+    FROM 
+        Production.Product
+    WHERE 
+        ListPrice > 1000
 );
 
 -- customers who purchased products costing more than $1000
