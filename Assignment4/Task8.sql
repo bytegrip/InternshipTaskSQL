0437f80ca7a6603476239f6eb434943a56bb5713
Daniel <59575049+bytegrip@users.noreply.github.com>
2025-05-23 14:10:12 +0300

    Update Task8.sql

--- Assignment4/Task8.sql
+++ Assignment4/Task8.sql
@@ -1,11 +1,20 @@
 USE AdventureWorks2022;
 
-SELECT e.BusinessEntityID, p.FirstName, p.LastName, e.HireDate
-FROM HumanResources.Employee e
-JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID
+SELECT 
+    e.BusinessEntityID, 
+    p.FirstName, 
+    p.LastName, 
+    e.HireDate
+FROM 
+    HumanResources.Employee e
+JOIN 
+    Person.Person p 
+    ON e.BusinessEntityID = p.BusinessEntityID
 WHERE e.HireDate < (
-    SELECT MAX(HireDate)
-    FROM HumanResources.Employee
+    SELECT 
+        MAX(HireDate)
+    FROM 
+        HumanResources.Employee
 );
 
 -- employees hired before the latest hire date
